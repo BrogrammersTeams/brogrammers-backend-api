18feee124bf680caff4830921bf5100116f75eca
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-03-09 15:08:53 -0800

    Refactored

--- controllers/Projects.js
+++ controllers/Projects.js
@@ -8,7 +8,8 @@ const getAllProjects = async (req, res) => {
 };
 
 const getProject = async (req, res) => {
-  let user = await User.find({ email: req.query.email });
+  const query = req.query;
+  let user = await User.find({ query });
 
   if (user.length) {
     return res.send(user);
