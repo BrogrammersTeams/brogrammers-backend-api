66f2ff22343887aa6a778a8de674d0255dae2a98
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-03-15 16:12:31 +0530

    Added  Python Executor

--- controllers/Home.js
+++ controllers/Home.js
@@ -1,23 +1,36 @@
+const { spawn } = require("child_process");
+
 const getAllHomes = (req, res) => {
-  res.send('Get request on /Home getAllHomes');
+  res.send("Get request on /Home getAllHomes");
 };
-  
+
 const getHome = (req, res) => {
-  res.send('Get request on /Home getHome');
+  res.send("Get request on /Home getHome");
 };
-  
+
 const createHome = (req, res) => {
-  res.send('Post request on /Home createHome');
+  res.send("Post request on /Home createHome");
 };
-  
-const updateHome = (req, res) => {
-  res.send('Patch request on /Home updateHome');
+
+const updateHome = async (req, res) => {
+  // export default async function handler(req, res) {
+  let python = spawn("python", ["hello.py"]);
+  let dataToSend = "";
+
+  for await (const data of python.stdout) {
+    //console.log(data.toString());
+    dataToSend += data.toString();
+  }
+  console.log(dataToSend);
+  //   return res.status(200).json({ message: dataToSend})
+  // }
+  res.send({ data: dataToSend.toString() });
 };
-  
+
 const deleteHome = (req, res) => {
-  res.send('Get request on /Home deleteHome');
+  res.send("Get request on /Home deleteHome");
 };
-  
+
 module.exports = {
   getHome,
   createHome,
--- routes/Home.js
+++ routes/Home.js
@@ -6,5 +6,6 @@ const route = express.Router();
 
 route.route("/").get(getAllHomes);
 route.route("/").post(createHome);
+route.route("/").patch(updateHome);
 
 module.exports = route;
